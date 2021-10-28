8a037b40bae86db3aee19f8c30847364c9dfd3a7
arseniy <arseniy.karmanov@mail.ru>
2021-10-29 01:07:01 +0700

    HW#6 init

--- src/main/java/ru/geekbrains/homeworks/App.java
+++ src/main/java/ru/geekbrains/homeworks/App.java
@@ -5,42 +5,6 @@ package ru.geekbrains.homeworks;
  */
 public class App {
     public static void main(String[] args) {
-        Person[] people = new Person[5];
 
-        people[0] = new Person(
-                "Ivanov Ivan",
-                "Engineer",
-                "ivivan@mailbox.com",
-                "892312312",
-                30000.0, 32);
-        people[1] = new Person(
-                "Ivanov Ivan",
-                "Engineer",
-                "ivivan@mailbox.com",
-                "892312312",
-                30000.0, 69);
-        people[2] = new Person(
-                "Ivanov Ivan",
-                "Engineer",
-                "ivivan@mailbox.com",
-                "892312312",
-                30000.0, 55);
-        people[3] = new Person(
-                "Ivanov Ivan",
-                "Engineer",
-                "ivivan@mailbox.com",
-                "892312312",
-                30000.0, 45);
-        people[4] = new Person(
-                "Ivanov Ivan",
-                "Engineer",
-                "ivivan@mailbox.com",
-                "892312312",
-                30000.0, 35);
-
-        for (Person person : people) {
-            if (person.getAge() > 40)
-                System.out.println(person);
-        }
     }
 }
