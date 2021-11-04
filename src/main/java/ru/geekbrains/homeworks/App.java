c65d41f88f352b6797203093f3c6cacd567e4aa3
arseniy <arseniy.karmanov@mail.ru>
2021-11-04 19:30:34 +0900

    HW7 done

--- src/main/java/ru/geekbrains/homeworks/App.java
+++ src/main/java/ru/geekbrains/homeworks/App.java
@@ -5,6 +5,25 @@ package ru.geekbrains.homeworks;
  */
 public class App {
     public static void main(String[] args) {
+        Cat[] cats = new Cat[] {
+            new Cat("cat1", 20),
+            new Cat("cat2", 40),
+            new Cat("cat3", 15),
+            new Cat("cat4", 60),
+            new Cat("cat5", 25),
+        };
+        Plate plate = new Plate(125);
 
+        for (Cat cat : cats) {
+            cat.eat(plate);
+            System.out.println(cat);
+        }
+
+        plate.addFoodCount(50);
+
+        for (Cat cat : cats) {
+            cat.eat(plate);
+            System.out.println(cat);
+        }
     }
 }
